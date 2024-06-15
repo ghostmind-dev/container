c52d4a57633e6425d5e9b6f9eea91e6b0b8b8e6d
Francois Seguin <seguin.mtl@gmail.com>
2024-06-15 03:07:32 +0000

    updating

--- Readme.md
+++ Readme.md
@@ -1,12 +1,3 @@
 # dvc
 
-:evergreen_tree: devcontainer docker image
 
-## publishing
-
-```bash
-./scripts/publish.mjs [options]
-
-# options:
-# --multiplatform      build for linux/amd64,linux/arm64
-```
---
+++ container/scripts/publish.ts
@@ -0,0 +1,22 @@
+import type { CustomArgs, CustomOptions } from 'jsr:@ghostmind/run';
+import { $, cd } from 'npm:zx@8.1.2';
+
+export default async function (_arg: CustomArgs, opts: CustomOptions) {
+  $.verbose = true;
+
+  const SRC = Deno.env.get('SRC') || 'src';
+
+  const CONTAINER = `${SRC}/container/app`;
+
+  const tagInstructions = ['-t ghcr.io/ghostmind-dev/dvc:latest'];
+
+  const tagInstructionsString = tagInstructions.join(' ');
+
+  const instructions = `docker buildx build --platform linux/amd64,linux/arm64 ${tagInstructionsString} --push ${CONTAINER}`;
+
+  // transfor the instructions into an array
+
+  const instructionsArray = instructions.split(' ');
+  await $`docker buildx create --use`;
+  await $`${instructionsArray}`;
+}
