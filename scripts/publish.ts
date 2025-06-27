3f726a7fd8ffa0a436596c83b00ac4e431999c53
Francois Seguin <admin@ghostmind.dev>
2025-06-27 02:55:21 +0000

    aws

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -5,29 +5,32 @@ export default async function (arg: CustomArgs, opts: CustomOptions) {
   $.verbose = true;
 
   const SRC = Deno.env.get('SRC');
-
   const { version } = opts.metaConfig;
-
   const CONTAINER = `${SRC}/container`;
 
-  // Start with default tags: latest and version
-  const tagInstructions = [
-    '-t ghcr.io/ghostmind-dev/dvc:latest',
-    `-t ghcr.io/ghostmind-dev/dvc:${version}`,
-  ];
+  // Get current git branch
+  const currentBranch = await $`git branch --show-current`.then((result: any) =>
+    result.stdout.trim()
+  );
 
-  // Add any additional tags provided by the user
-  if (arg && Array.isArray(arg)) {
-    for (const additionalTag of arg) {
-      tagInstructions.push(`-t ghcr.io/ghostmind-dev/dvc:${additionalTag}`);
-    }
+  let tagInstructions: string[] = [];
+
+  if (currentBranch === 'main') {
+    // Main: latest + version
+    tagInstructions = [
+      '-t ghcr.io/ghostmind-dev/dvc:latest',
+      `-t ghcr.io/ghostmind-dev/dvc:${version}`,
+    ];
+  } else {
+    // Any other branch: just "dev"
+    tagInstructions = ['-t ghcr.io/ghostmind-dev/dvc:dev'];
   }
 
   const tagInstructionsString = tagInstructions.join(' ');
-
   const instructions = `docker buildx build --platform linux/amd64,linux/arm64 ${tagInstructionsString} --push ${CONTAINER}`;
 
-  // Print the tags that will be included
+  // Print branch and tagging information
+  console.log(`Current branch: ${currentBranch}`);
   console.log('Tags that will be included:');
   tagInstructions.forEach((tag) => {
     const tagName = tag.replace('-t ghcr.io/ghostmind-dev/dvc:', '');
